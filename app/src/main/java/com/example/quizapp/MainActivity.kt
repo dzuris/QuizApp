31bfca8221d8014d838c30a607118a22258b2568
Adam Admin <adamdzurilla19@gmail.com>
2020-05-15 13:17:53 +0200

    Changed main activity variable

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -10,6 +10,7 @@ import com.example.quizapp.database.DatabaseHelper
 class MainActivity : AppCompatActivity() {
 
     private var db: DatabaseHelper? = null
+    private var testGithub: Int = 10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
